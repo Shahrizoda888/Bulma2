c5dd51d668ac299aefef0c6b66f8d112818c2b08
shahrizodasub <shahrizodasubhonova2006@gmail.com>
2021-08-12 19:24:12 +0500

    index.js qo'shildi

---
+++ index.js
@@ -0,0 +1,9 @@
+const burgerIcon = document.querySelector('#burger');
+const navbarmenu = document.querySelector('#nav-links');
+burgerIcon.addEventListener('click', () => {
+    navbarmenu.classList.toggle('is-active');
+})
+
+
+
+
